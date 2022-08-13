3dad94e61f0e6bf0047fb69fa6041f5bc27df16f
GalloNa <107970983+GalloNa@users.noreply.github.com>
2022-08-13 03:02:24 -0300

    Ejercicios valoración final
    Ejercicios valoración final Introducción a la programación.

---
+++ programador.py
@@ -0,0 +1,54 @@
+#1) Crear una lista en Python denominada “Dueno” que contenga los siguientes valores:#
+
+Dueno=[28957346,"Juan","Perez",4789689,"Belgrano 101"]
+if Dueno[0]>26000000:
+    print("El telefono del cliente es:", Dueno[3]);
+
+
+#------------------------------------------------------------
+
+#3)Crear un lista en Python denominado “Perro”
+
+#Perro=[13,"Puppy", "12/12/2012","Macho", 123]
+
+#Perro[2] = "13/12/2012";
+#del Perro[4];
+#Perro.append(28957346)
+
+#print(Perro)
+
+#------------------------------------------------------------
+
+#4)Crear un lista en Python denominado “Perro2” que contenga los siguientes valores:
+
+#Perro2=[14,"Fido","12/12/2012","Macho",23546987]
+#Perro2.reverse()
+
+#print(Perro2)
+
+#------------------------------------------------------------
+
+#5)Crear una tupla en Python con el nombre de “Historial” la cual contenga los siguientes valores:
+
+#Historial=(2350, 5960, 23000, 1000, 8900)
+
+#gastosAtencion=sum(Historial);
+#if gastosAtencion < 30000:
+#    print(gastosAtencion)
+#else:
+#    print("Gastos por encima de lo presupuestado")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
